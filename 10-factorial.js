a21111312782426de91d6900cc7f8e85784e9d41
Godwinakladzi <godwinakladzi@gmail.com>
2025-12-14 03:50:27 +0000

    added factorial code

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+// Get the first argument and convert to integer
+const num = parseInt(process.argv[2], 10);
+
+// Recursive function to compute factorial
+function factorial(n) {
+    if (isNaN(n) || n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+// Print the factorial of the input number
+console.log(factorial(num));
